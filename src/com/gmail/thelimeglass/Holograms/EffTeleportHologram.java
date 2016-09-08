01734ca2b3c3a912d65332824f3ae08831c1495e
TheLimeGlass <seantgrover@gmail.com>
2016-09-08 11:11:53 -0600

    Add files via upload

---
+++ src/com/gmail/thelimeglass/Holograms/EffTeleportHologram.java
@@ -0,0 +1,34 @@
+package com.gmail.thelimeglass.Holograms;
+
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+import javax.annotation.Nullable;
+import org.bukkit.Location;
+import org.bukkit.event.Event;
+
+import com.gmail.thelimeglass.SkellettAPI.SkellettHolograms;
+
+public class EffTeleportHologram extends Effect {
+
+	//[skellett] (tp|teleport|move) holo[gram] [(with|and|of)] id %integer% [to] [location] %location%
+	
+	private Expression<Integer> ID;
+	private Expression<Location> loc;
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] e, int arg1, Kleenean arg2, ParseResult arg3) {
+		ID = (Expression<Integer>) e[0];
+		loc = (Expression<Location>) e[1];
+		return true;
+	}
+	@Override
+	public String toString(@Nullable Event paramEvent, boolean paramBoolean) {
+		return "[skellett] (tp|teleport|move) holo[gram] [(with|and|of)] id %integer% [to] [location] %location%";
+	}
+	@Override
+	protected void execute(Event e) {
+		SkellettHolograms.teleportHologram(ID.getSingle(e), loc.getSingle(e));
+	}
+}
